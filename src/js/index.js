b1190962da4ab65c8e18e421ec83d5e4140a3c90
Cindy Juarez <sceendy@gmail.com>
2018-02-20 14:41:25 -0600

    1.0.1 (#1)
    * added: image optimization
    
    * added: rendering of image in html file
    
    * fixed image filename, rendering image as first child

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
 import style from '../scss/_styles.scss';
+
+const image = require('../assets/images/placeholder.jpeg');
+const imageContainer = document.getElementsByClassName('image')[0];
+
+if (imageContainer) {
+  const imageEl = document.createElement('img');
+  imageEl.setAttribute('src', `${image}`);
+  imageContainer.insertAdjacentElement('afterbegin', imageEl);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,18 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+            },
+          },
+        ],
       }
     ]
   },
