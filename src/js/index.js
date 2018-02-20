87a2d97ba245eed13e3cfa2b65a36e1ed89c2d5b
Cindy Juarez <sceendy@gmail.com>
2018-02-20 14:25:32 -0600

    added: rendering of image in html file

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 import style from '../scss/_styles.scss';
+
+const image = require('../assets/images/placeholder.jpg');
+const imageContainer = document.getElementsByClassName('image')[0];
+
+if (imageContainer) {
+  imageContainer.innerHTML = `<img src=${image} />`;
+}
